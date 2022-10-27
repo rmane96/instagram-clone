435c8a2dbcae235e9c02090a3f5fa3fbe59fd859
rmane96 <rmane9641@gmail.com>
2022-10-27 19:15:30 +0530

    password encryption

--- db/db_user.py
+++ db/db_user.py
@@ -1,17 +1,20 @@
 from db.models import UserModel
 from routers.schemas import UserBase
 from sqlalchemy.orm import Session
+from db.hashing import Hash
 
 def create_user(db:Session,request:UserBase):
     new_user = UserModel(
         username = request.username,
         email = request.email,
-        password = request.password
+        password = Hash.bcrypt(request.password)
     )
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
     return new_user
+
+
 
 
 
